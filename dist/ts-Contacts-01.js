8c9d4fc04db810bfdd560a36125547cb167b05a4
Dima163163 <khlopovdima@gmail.com>
2024-02-22 12:40:30 +0400

    lesson 2.1

---
+++ dist/ts-Contacts-01.js
@@ -0,0 +1,28 @@
+"use strict";
+const persons = [
+    {
+        name: 'Иван Петров',
+        age: 27,
+        group: 'SEO-специалист',
+    },
+    {
+        name: 'Марат Aляуддинов',
+        age: 20,
+        group: 'Музыкант',
+    },
+    {
+        name: 'Иван Иванов',
+        age: 23,
+        group: 'Семья',
+    },
+    {
+        name: 'Макс Максимов',
+        age: 29,
+        group: 'Друзья',
+    }
+];
+const logPerson = (user) => {
+    console.log(`${user.name}, ${user.age}`);
+};
+console.log('Users:');
+persons.forEach(logPerson);
